95286b8780b87775bb16b498b5093d221cec786a
Alek Slater <skela@davincium.com>
2019-01-07 23:05:26 +0100

    Create lightsail-compose.sh

---
+++ lightsail-compose.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# make it so you don't need to sudo to run docker commands
+usermod -aG docker ubuntu
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# copy the dockerfile into /srv/docker 
+# if you change this, change the systemd service file to match
+# WorkingDirectory=[whatever you have below]
+mkdir /srv/docker
+echo "
+
+version: "3.2"
+
+services:
+
+        site:
+                image: davincium/site:latest
+                ports:
+                        - "80:80"
+
+" > /srv/docker/docker-compose.yml
+
+# copy in systemd unit file and register it so our compose file runs 
+# on system restart
+
+echo "
+# /etc/systemd/system/docker-compose-app.service
+# thanks to oleg belostotsky on stack overflow for this 
+
+[Unit]
+Description=Docker Compose Application Service
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+# match the below to wherever you copied your docker-compose.yml
+WorkingDirectory=/srv/docker
+ExecStart=/usr/local/bin/docker-compose up -d
+ExecStop=/usr/local/bin/docker-compose down
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/docker-compose-app.service
+systemctl enable docker-compose-app
+
+# start up the application via docker-compose
+docker-compose -f /srv/docker/docker-compose.yml up -d
